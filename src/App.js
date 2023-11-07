217b051d09d238de1ce29ed85e6093893603a1f1
Soumyadeb2000 <suklasutradhar1976@gmail.com>
2023-11-07 20:57:47 +0530

    React Assignment 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Lets Start!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  )
+}
+
+export default App;
