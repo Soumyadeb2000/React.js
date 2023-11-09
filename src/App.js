c7d25ae7cb57cdb4f5969193e34e9fe67f5f8f21
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-11-09 11:48:55 +0530

    Passing data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      expenseDate: new Date(2023, 9, 28),
+      expenseTitle: "food",
+      expenseAmount: 25,
+      expenseLocation: "Lucknow"
+    },
+    {
+      expenseDate: new Date(2023, 9, 28),
+      expenseTitle: "drink",
+      expenseAmount: 50,
+      expenseLocation: "Lucknow"
+    },
+    {
+      expenseDate: new Date(2023, 9, 28),
+      expenseTitle: "Car Insurance",
+      expenseAmount: 250,
+      expenseLocation: "Lucknow"
+    }, {
+      expenseDate: new Date(2023, 9, 28),
+      expenseTitle: "petrol",
+      expenseAmount: 100,
+      expenseLocation: "Lucknow"
+    }];
   return (
     <div>
       <h2>Lets Start!</h2>
-      <ExpenseItem></ExpenseItem>
+      {
+        expenses.map((expense, i) => 
+          <ExpenseItem date={
+            expense.expenseDate
+          } title={
+            expense.expenseTitle
+          } amount={
+            expense.expenseAmount
+          } location={
+            expense.expenseLocation
+          }></ExpenseItem>
+        )
+      }
     </div>
   )
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    
     return (
-        <div>
-            <div>Food   Rs 10</div>
-            <div>Petrol   Rs 100</div>
-            <div>Movies   Rs 200</div>
+        <div className='expense-item'>
+            <div>{props.date.toLocaleString()}</div>
+            <div>
+                <h2 className='expense_item__description'>{props.title}</h2>
+                <div className='expense_item__price'>Rs. {props.amount}</div>
+                <h2 className='expense_item__location'>{props.location}</h2>
+            </div>
         </div>
     )
 }
