dad1508e7f3f64447677a46dd705d52640e4ee66
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-11-15 01:20:58 +0530

    Added delete expense button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,22 @@
 import '../Expenses/ExpenseItem.css'
 import ExpenseDate from '../Expenses/ExpenseDate';
 import ExpenseDetails from '../Expenses/ExpenseDetails';
-const ExpenseItem = (props) => {
+
     
-    return (
+
+const ExpenseItem = (props) => {
+    const deleteExpense = (e) => {
+        e.preventDefault();
+        const expenseDiv = e.target.parentElement;
+        const parent = expenseDiv.parentElement;
+        parent.removeChild(expenseDiv)
+        
+    }
+    return (  
         <div className='expense-item'>
             <ExpenseDate date={props.date}/>
             <ExpenseDetails title={props.title} amount={props.amount} location={props.location}/>
+            <button onClick={deleteExpense}>Delete Expense</button>
         </div>
     )
 }
