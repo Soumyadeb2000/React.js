60e59a0190818f9a61029214bde67b632d0a3a41
Soumyadeb2000 <soumyadebsutradhar@gmail.com>
2023-11-24 20:23:34 +0530

    submitting the form

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,19 +1,41 @@
+import { useState } from "react";
 
 const ExpenseForm = () => {
+    const [enteredTitle , setEnteredTitle] = useState('Initial');
+    const [enteredAmount , setEnteredAmount] = useState('Initial');
+    const [enteredDate , setEnteredDate] = useState('Initial')
+    function updateTitle(e) {
+        setEnteredTitle(e.target.value)
+        console.log(enteredTitle);
+    }
+    function updateAmount(e) {
+        setEnteredAmount(e.target.value)
+        console.log(enteredAmount);
+    }
+    function updateDate(e) {
+        setEnteredDate(e.target.value)
+        console.log(enteredDate);
+    }
+
+    function display(e) {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const amount = e.target.amount.value;
+        const date = e.target.title.date;
+        const obj = {title, amount, date};
+        console.log(obj);
+    }
+
     return (
         <div>
-            <form>
-                <input onChange={display} id="title" name="title" type="text" placeholder="Expense Title"></input>
-                <input onChange={display} id="amount" name="amount" type="number" placeholder="Expense Amount"></input>
-                <input onChange={display} id="date" name="date" type="date" placeholder="Expense Date"></input>
+            <form onSubmit={display}>
+                <input onChange={updateTitle} id="title" name="title" type="text" placeholder="Expense Title"></input>
+                <input onChange={updateAmount} id="amount" name="amount" type="number" placeholder="Expense Amount"></input>
+                <input onChange={updateDate} id="date" name="date" type="date" placeholder="Expense Date"></input>
                 <button type="submit">submit</button>
             </form>
         </div>
     )
-}
-
-function display(e) {
-    console.log(e.target.value);
 }
 
 export default ExpenseForm;
